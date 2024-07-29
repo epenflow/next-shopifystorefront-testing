e2e1d060eca2fdc4011ee20d0ecbb427b1fc046c
Epen Flow <53342177+epenflow@users.noreply.github.com>
2024-07-29 23:42:31 +0800

    add query GET_COLLECTIONS

--- schema/queries.ts
+++ schema/queries.ts
@@ -22,7 +22,13 @@ export const GET_PRODUCTS = gql`
 export const GET_COLLECTION_BY_ID = gql`
 	query GetCollectionById($id: ID!) {
 		collection(id: $id) {
+			title
 			handle
+			descriptionHtml
+			image {
+				url
+				altText
+			}
 			image {
 				url
 				altText
@@ -40,6 +46,24 @@ export const GET_COLLECTION_BY_ID = gql`
 							url
 						}
 					}
+				}
+			}
+		}
+	}
+`;
+export const GET_COLLECTIONS = gql`
+	query GetCollections($first: Int!, $query: String) {
+		collections(first: $first, query: $query) {
+			nodes {
+				title
+				id
+				image {
+					url
+					altText
+				}
+				seo {
+					title
+					description
 				}
 			}
 		}
