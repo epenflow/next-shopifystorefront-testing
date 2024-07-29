ce807ee7fd250f056d363160421c9ee64f89109b
Epen Flow <53342177+epenflow@users.noreply.github.com>
2024-07-29 16:24:43 +0800

    codegen --config --shopify-domain --shopify-access-token

--- codegen.ts
+++ codegen.ts
@@ -1,7 +1,27 @@
 import { CodegenConfig } from '@graphql-codegen/cli';
-
+import dotenv from 'dotenv';
+dotenv.config();
+/**
+ *
+ *
+ */
+const shopify_domain = `https://${process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN}${process.env.NEXT_PUBLIC_SHOPIFY_API_VERSION}`;
+const shopify_access_token = `${process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN}`;
+const header = `${process.env.NEXT_PUBLIC_SHOPIFY_HEADER}`;
+/**
+ *
+ *
+ */
 const config: CodegenConfig = {
-	schema: `https://${process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN}${process.env.NEXT_PUBLIC_SHOPIFY_API_VERSION}`,
+	schema: [
+		{
+			[shopify_domain]: {
+				headers: {
+					[header]: shopify_access_token,
+				},
+			},
+		},
+	],
 	documents: './schema/queries.ts',
 	generates: {
 		'./libs/generated/graphql.tsx': {
