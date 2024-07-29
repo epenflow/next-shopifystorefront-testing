aee2f0a2b8ea0cbbc19b709784fa65a5d7d428ab
Epen Flow <53342177+epenflow@users.noreply.github.com>
2024-07-29 17:44:29 +0800

    GET_COLLECTION_BY_ID query

--- schema/queries.ts
+++ schema/queries.ts
@@ -19,3 +19,28 @@ export const GET_PRODUCTS = gql`
 		}
 	}
 `;
+export const GET_COLLECTION_BY_ID = gql`
+	query GetCollectionById($id: ID!) {
+		collection(id: $id) {
+			handle
+			image {
+				url
+			}
+			seo {
+				description
+				title
+			}
+			products(first: 10) {
+				nodes {
+					id
+					title
+					images(first: 10) {
+						nodes {
+							url
+						}
+					}
+				}
+			}
+		}
+	}
+`;
