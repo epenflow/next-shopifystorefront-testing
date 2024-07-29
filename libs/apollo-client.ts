c95a974952328a2ce715580035d4831e4cee7c75
Epen Flow <53342177+epenflow@users.noreply.github.com>
2024-07-29 16:25:41 +0800

    changes shopify api --version

--- libs/apollo-client.ts
+++ libs/apollo-client.ts
@@ -1,13 +1,14 @@
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+console.log('API Version', process.env.NEXT_PUBLIC_SHOPIFY_API_VERSION);
 console.log('Store Domain:', process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN);
 console.log(
 	'Access Token:',
 	process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN,
 );
-
 const client = new ApolloClient({
 	link: new HttpLink({
-		uri: `https://${process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN}/api/2024-07/graphql.json`,
+		uri: `https://
+		${process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN}${process.env.NEXT_PUBLIC_SHOPIFY_API_VERSION}`,
 		headers: {
 			'X-Shopify-Storefront-Access-Token': `${process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN}`,
 		},
