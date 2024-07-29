2b302347a63549183e8dd04bab8ff991d6891b11
Epen Flow <53342177+epenflow@users.noreply.github.com>
2024-07-29 17:47:08 +0800

    add collection

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import { Collection } from '@/app/__components__/collection';
 import { Products } from '@/app/__components__/products';
-import Image from 'next/image';
 
 export default function Home() {
 	return (
 		<main>
 			<h1>test</h1>
+			<Collection />
 			<Products />
 		</main>
 	);
