b255655b42e2d6ff925b0bfa8b5c303792d233a6
Epen Flow <53342177+epenflow@users.noreply.github.com>
2024-07-30 17:30:08 +0800

    suppressHydrationWarning

--- app/(store)/layout.tsx
+++ app/(store)/layout.tsx
@@ -1,8 +1,9 @@
+import { ContextWrapper } from '@/lib/context-wrapper';
 import React from 'react';
 interface StoreLayoutProps {
 	children: React.ReactNode;
 }
 
 export default async function StoreLayout({ children }: StoreLayoutProps) {
-	return children;
+	return <ContextWrapper>{children}</ContextWrapper>;
 }
