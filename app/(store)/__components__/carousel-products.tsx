47fb4ba573a114492bbb5d46c73571c8b1cfbedb
Epen Flow <53342177+epenflow@users.noreply.github.com>
2024-07-30 23:42:02 +0800

    refactors

--- app/(store)/__components__/carousel-products.tsx
+++ app/(store)/__components__/carousel-products.tsx
@@ -11,6 +11,9 @@ import {
 	CarouselItem,
 } from '@/components/ui/carousel';
 import { MappingWrapper } from '@/lib/mapping-wrapper';
+import Image from 'next/image';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 const variables: GetCollectionsQueryVariables = {
 	first: 10,
 	query: "-'home page'",
@@ -22,36 +25,37 @@ export const CarouselProducts = () => {
 	console.log(data);
 	return (
 		<Carousel
-			className="w-full h-full lg:w-4/5 m-auto border-x"
 			opts={{
 				loop: true,
 				dragFree: true,
 				dragThreshold: 10,
 				skipSnaps: true,
 			}}>
-			<CarouselContent className="gap-2 pl-2">
+			<CarouselContent className="gap-2 pl-2 lg:gap-5 lg:pl-5">
 				<MappingWrapper
 					items={data?.collections.nodes}
 					render={(collection) => (
 						<CarouselItem
 							key={collection.id}
-							className="basis-1/2 h-64 lg:h-96 relative flex items-end overflow-hidden bg-cover bg-center bg-no-repeat border justify-center p-2.5"
-							style={{
-								backgroundImage: `url("${collection.image?.url})`,
-							}}>
-							<div
-								aria-label={collection.title}
-								className="uppercase w-full flex flex-col items-end">
-								<h1 className="text-start text-lg lg:text-4xl">
+							className="basis-4/5 lg:basis-1/2 flex-shrink-0 relative p-0 bg-card">
+							<Image
+								src={collection.image?.url}
+								alt={`${collection.image?.url}`}
+								sizes="100vw"
+								width={0}
+								height={0}
+								className="w-full h-60 lg:h-96 object-cover object-center border"
+							/>
+							<div className="w-full flex text-end p-2 font-medium flex-col justify-end items-end capitalize border border-t-0">
+								<h1 className="text-foreground/95 text-2xl">
 									{collection.title}
 								</h1>
+								<Link
+									className="text-sm text-foreground/85 hover:underline"
+									href={'/'}>
+									shop now
+								</Link>
 							</div>
-							<p
-								aria-hidden
-								hidden
-								aria-label={`${collection.seo.title}`}>
-								{collection.seo.description}
-							</p>
 						</CarouselItem>
 					)}
 				/>
