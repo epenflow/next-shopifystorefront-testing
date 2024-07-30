3ec61b54a0e97afba0d0b1ff407d6a0eba8f90d5
Epen Flow <53342177+epenflow@users.noreply.github.com>
2024-07-30 23:43:04 +0800

    adds hero and carousel-journal

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,9 +1,15 @@
+import { CarouselJournal } from '@/app/(store)/__components__/carousel-journal';
 import { CarouselProducts } from '@/app/(store)/__components__/carousel-products';
+import { Hero } from '@/app/(store)/__components__/hero';
 
 export default function Home() {
 	return (
-		<main>
+		<main className="space-y-2.5 overflow-x-hidden">
+			<Hero />
+			<h1 className="text-5xl px-2 lg:px-4">Explore The Collection</h1>
 			<CarouselProducts />
+			<h1 className="text-5xl px-2 lg:px-4">Journal</h1>
+			<CarouselJournal />
 		</main>
 	);
 }
