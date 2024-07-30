10a88d930df22bc7c3cd5c89d70c0abbed496571
Epen Flow <53342177+epenflow@users.noreply.github.com>
2024-07-30 23:42:21 +0800

    init

---
+++ app/(store)/__components__/hero.tsx
@@ -0,0 +1,15 @@
+export const Hero = () => {
+	return (
+		<div className="h-screen w-screen flex items-center justify-center flex-col space-y-3 bg-black text-white">
+			<h1 className="text-center px-4 text-2xl font-medium md:w-4/5 lg:w-1/2">
+				Elegance Redefined: Discover Your Perfect Jewelry Piece
+			</h1>
+			<p className="text-justify px-5 lg:w-1/2 md:w-4/5">
+				Welcome to Our Exclusive Jewelry Boutique! Discover the perfect
+				blend of elegance and everyday wear with our curated collection
+				of fine jewelry. Whether you’re looking for a statement piece or
+				a subtle accessory, we have something for everyone.
+			</p>
+		</div>
+	);
+};
