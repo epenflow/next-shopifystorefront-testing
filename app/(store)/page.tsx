79acbfafb9a192b696e4f77db73086ec15ac37d3
Epen Flow <53342177+epenflow@users.noreply.github.com>
2024-07-31 15:32:57 +0800

    add RecommendationCard

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,6 +1,7 @@
 import { CarouselJournal } from '@/app/(store)/__components__/carousel-journal';
 import { CarouselProducts } from '@/app/(store)/__components__/carousel-products';
 import { Hero } from '@/app/(store)/__components__/hero';
+import { RecommendationCard } from '@/app/(store)/__components__/recommendation-card';
 
 export default function Home() {
 	return (
@@ -8,6 +9,7 @@ export default function Home() {
 			<Hero />
 			<h1 className="text-5xl px-2 lg:px-4">Explore The Collection</h1>
 			<CarouselProducts />
+			<RecommendationCard />
 			<h1 className="text-5xl px-2 lg:px-4">Journal</h1>
 			<CarouselJournal />
 		</main>
