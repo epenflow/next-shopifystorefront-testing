61395518a1ef99ed61ff7e9dc204f6f26b369a9d
Epen Flow <53342177+epenflow@users.noreply.github.com>
2024-07-31 15:33:54 +0800

    init

--- app/(store)/__components__/navbar.tsx
+++ app/(store)/__components__/navbar.tsx
@@ -1,6 +1,6 @@
 export const Navbar = () => {
 	return (
-		<header className="w-screen fixed top-0 left-1/2 -translate-x-1/2 px-5 py-2.5 lg:px-40">
+		<header className="w-screen fixed top-0 left-1/2 -translate-x-1/2 px-5 py-2.5 lg:px-40 z-50">
 			<nav className="w-full bg-card rounded-md flex justify-between items-center py-2.5 px-4">
 				<h1>Test</h1>
 				<h1>Tes</h1>
